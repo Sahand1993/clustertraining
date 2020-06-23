66ca327b242ddafb2833721b368f39feb557565c
Sahand1993 <sahandz@hotmail.com>
2020-06-23 15:05:45 +0200

    add get model path

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,5 +1,7 @@
+import re
 import tensorflow as tf
 import numpy as np
+import os
 
 def cosine_similarity(a, b):
     c = tf.compat.v1.sqrt(tf.compat.v1.reduce_sum(tf.compat.v1.multiply(a, a), axis=1))
@@ -27,3 +29,11 @@ def correct_guesses_of_dssm_10(sess, feed_dict, prob_p, prob_n1, prob_n2, prob_n
     m = np.vstack((p, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10))
     guess = np.argmax(m, 0)
     return len(np.where(guess == 0)[0])
+
+pattern = re.compile("dssm-([0-9]+).*")
+def get_model_path(modelHomePath):
+    tfDir = os.path.join(modelHomePath, "model_bs16_lr0.00011702251629896198", "tf")
+    dssmModelFiles = list(filter(lambda fileName: fileName != "checkpoint", os.listdir(tfDir)))
+    modelNo = pattern.match(dssmModelFiles[0]).group(1)
+    modelPath = os.path.join(modelHomePath, "model_bs16_lr0.00011702251629896198", "tf", "dssm-{}".format(modelNo))
+    return modelPath
